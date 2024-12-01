f82ca9c839d095df37c087cbd5e6d81c5441b76a
Nico Baier <nico.baier@gmail.com>
2024-12-01 17:50:08 -0600

    feat(src/solutions/01_solution.ts): added day 1 solution

---
+++ src/solutions/01_solution.ts
@@ -0,0 +1,28 @@
+export default async function (data: string) {
+	const pairs = data.trim().split("\n");
+
+	const left: number[] = [];
+	const right: number[] = [];
+
+	for (const pair of pairs) {
+		const splitPair = pair.replace(/\s+/, " ").split(" ");
+		left.push(Number.parseInt(splitPair[0]));
+		right.push(Number.parseInt(splitPair[1]));
+	}
+
+	const leftSorted = left.sort();
+	const rightSorted = right.sort();
+
+	const absoluteDifferences = leftSorted.map((num, index) => {
+		return Math.abs(num - rightSorted[index]);
+	});
+
+	const totalDifference = absoluteDifferences.reduce(
+		(accumulator, currentValue) => {
+			return accumulator + currentValue;
+		},
+		0,
+	);
+
+	return totalDifference;
+}
