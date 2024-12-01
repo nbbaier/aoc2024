1d65e1ef85e7c754e09146dac856813492c56c5f
Nico Baier <nico.baier@gmail.com>
2024-12-01 17:47:10 -0600

    feat(src/utils.ts): added function to fetch aoc data

---
+++ src/utils.ts
@@ -0,0 +1,20 @@
+export const getAocData = async (day: number, year: number) => {
+	return await fetch(`https://adventofcode.com/${year}/day/${day}/input`, {
+		headers: {
+			Cookie: Bun.env.AOC_TOKEN,
+		},
+	});
+};
+
+import path from "node:path";
+
+export async function loadData(day: number, example = false) {
+	const dayString = String(day).padStart(2, "0");
+	const fileName = example
+		? `${dayString}_example.txt`
+		: `${dayString}_data.txt`;
+	const dataPath = path.join(Bun.env.DATA, fileName);
+	const file = Bun.file(dataPath);
+
+	return await file.text();
+}
