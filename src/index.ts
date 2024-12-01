9caf22aa22e60e0ebd89c211c1ce7d3627af50c2
Nico Baier <nico.baier@gmail.com>
2024-12-01 17:51:07 -0600

    feat(src/index.ts): implemented cli

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,47 @@
-import { loadData } from "./utils/loader";
+import { getAocData, loadData } from "@/utils";
 
-console.log(loadData("01"));
+const commands = ["try", "test", "fetch"];
+
+function validate(args: string[]) {
+	if (!commands.includes(args[0])) {
+		throw new Error(`Use one of these commands: ${commands.join(", ")}`);
+	}
+	if (args.length < 2) {
+		throw new Error("You didn't supply a day");
+	}
+}
+
+async function main() {
+	const [command, dayArg] = Bun.argv.slice(2);
+	const day = Number.parseInt(dayArg);
+	validate([command, dayArg]);
+
+	if (command === "fetch") {
+		const year = 2024;
+		try {
+			const res = await getAocData(day, year);
+			const data = await res.text();
+			await Bun.write(
+				`${Bun.env.DATA_DIR}/${String(day).padStart(2, "0")}_data.txt`,
+				data,
+			);
+			return;
+		} catch (error) {
+			console.error(error);
+			return;
+		}
+	}
+
+	const mode = command === "test";
+	const data = await loadData(day, mode);
+	const answer = await import(
+		`@/solutions/${String(day).padStart(2, "0")}_solution`
+	);
+	console.log(await answer.default(data));
+}
+
+main().catch((error) => {
+	if (error instanceof Error) {
+		console.error(error.message);
+	}
+});
