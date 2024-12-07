d4a55020737a0ea7ee9c576ac0c4a98910a5620d
Nico Baier <nico.baier@gmail.com>
2024-12-06 23:14:33 -0600

    feat: add day 2 part 2

--- src/solutions/02_solution.ts
+++ src/solutions/02_solution.ts
@@ -1,30 +1,65 @@
-function part1(data: string) {
-	const reports = data
+function parseReports(data: string) {
+	return data
 		.trim()
 		.split("\n")
 		.map((r) => r.split(" ").map((i) => Number.parseInt(i)));
+}
+
+function checkReport(report: number[]): boolean {
+	const directions: string[] = [];
+
+	for (let i = 0; i < report.length - 1; i++) {
+		const diff = report[i] - report[i + 1];
+
+		if (Math.abs(diff) < 1 || Math.abs(diff) > 3) {
+			return false;
+		}
+
+		directions.push(diff < 0 ? "down" : "up");
+	}
+
+	return new Set(directions).size === 1;
+}
+
+function dropOne<T>(arr: T[]) {
+	return arr.map((_, index) =>
+		arr.filter((_, filterIndex) => filterIndex !== index),
+	);
+}
+
+function part1(data: string) {
+	const reports = parseReports(data);
 
 	let count = 0;
 
 	for (const report of reports) {
-		const directions: string[] = [];
-
-		for (let i = 0; i < report.length - 1; i++) {
-			const diff = report[i] - report[i + 1];
-
-			if (Math.abs(diff) < 1 || Math.abs(diff) > 3) {
-				directions.length = 0;
-				break;
-			}
-
-			directions.push(diff < 0 ? "down" : "up");
-		}
-
-		if (directions.length > 0 && new Set(directions).size === 1) {
-			count++;
-		}
+		const safe = checkReport(report);
+		safe ? count++ : null;
 	}
+
 	return count;
 }
 
-export default { 1: part1 };
+function part2(data: string) {
+	const reports = parseReports(data);
+
+	let count = 0;
+
+	for (const report of reports) {
+		const safe = checkReport(report);
+		if (safe) {
+			count++;
+		} else {
+			const altReports = dropOne(report);
+			const altResults = [];
+			for (const altReport of altReports) {
+				altResults.push(checkReport(altReport));
+			}
+			altResults.some(Boolean) ? count++ : null;
+		}
+	}
+
+	return count;
+}
+
+export default { 1: part1, 2: part2 };
