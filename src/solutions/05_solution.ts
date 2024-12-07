55a64a95720b336ea1578ae517a8dc6a09c08f84
Nico Baier <nico.baier@gmail.com>
2024-12-07 13:04:46 -0600

    feat: add day 5 part 2

--- src/solutions/05_solution.ts
+++ src/solutions/05_solution.ts
@@ -1,10 +1,4 @@
-function checkOrder(arr: string[], rule: string) {
-	const [first, second] = rule.split("|");
-	if (!arr.includes(first) || !arr.includes(second)) {
-		return null;
-	}
-	return arr.indexOf(first) < arr.indexOf(second);
-}
+import toposort from "toposort";
 
 function getMiddleElement<T>(arr: T[]): T | null {
 	if (arr.length === 0) {
@@ -14,35 +8,38 @@ function getMiddleElement<T>(arr: T[]): T | null {
 	return arr[middleIndex];
 }
 
-function validateUpdates(updates: string[][], rules: string[]) {
-	const correctUpdates: string[][] = [];
-	const incorrectUpdates: string[][] = [];
+function validateUpdate(update: string[], rules: string[]) {
+	const ruleResults = [];
 
-	for (const update of updates) {
-		const ruleResults = [];
-		for (const rule of rules) {
-			const res = checkOrder(update, rule);
-			ruleResults.push(res);
-		}
-		const verdict = ruleResults.filter((r) => r !== null).every(Boolean);
-		if (verdict) {
-			correctUpdates.push(update);
-		} else {
-			incorrectUpdates.push(update);
-		}
+	for (const rule of rules) {
+		const res = checkOrder(update, rule);
+		ruleResults.push(res);
 	}
-
-	return { correctUpdates, incorrectUpdates };
+	return ruleResults.filter((r) => r !== null).every(Boolean);
 }
 
-function part1(data: string) {
+function checkOrder(arr: string[], rule: string) {
+	const [first, second] = rule.split("|");
+	if (!arr.includes(first) || !arr.includes(second)) {
+		return null;
+	}
+	return arr.indexOf(first) < arr.indexOf(second);
+}
+
+function parse(data: string) {
 	const contents = data.trim().split("\n\n");
 	const rules = contents[0].split("\n");
 	const updates = contents[1].split("\n").map((update) => update.split(","));
+	return { rules, updates };
+}
 
-	const { correctUpdates } = validateUpdates(updates, rules);
+function part1(data: string) {
+	const { rules, updates } = parse(data);
 
-	return correctUpdates
+	return updates
+		.filter((update) => {
+			return validateUpdate(update, rules);
+		})
 		.map((update) => getMiddleElement(update))
 		.reduce((acc, curr) => {
 			return acc + (curr !== null ? Number.parseInt(curr) : 0);
@@ -50,13 +47,36 @@ function part1(data: string) {
 }
 
 function part2(data: string) {
-	const contents = data.trim().split("\n\n");
-	const rules = contents[0].split("\n");
-	const updates = contents[1].split("\n").map((update) => update.split(","));
+	const { rules, updates } = parse(data);
 
-	const { incorrectUpdates } = validateUpdates(updates, rules);
+	// get the rulesets for incorrectly ordered updates
+	const incorrect = updates
+		.filter((update) => !validateUpdate(update, rules))
+		.map((update) => {
+			return rules.filter((rule) => {
+				const [first, second] = rule.split("|");
+				return update.includes(first) && update.includes(second);
+			});
+		});
 
-	return incorrectUpdates;
+	// get the corrected updates by using toposort
+	const corrected = incorrect
+		.map((update) => {
+			// create a graph from the update rulesets
+			const graph: [string, string][] = update.map((rule) => {
+				const [from, to] = rule.split("|");
+				return [from, to];
+			});
+
+			return graph;
+		})
+		.map((graph) => toposort(graph));
+
+	return corrected
+		.map((update) => getMiddleElement(update))
+		.reduce((acc, curr) => {
+			return acc + (curr !== null ? Number.parseInt(curr) : 0);
+		}, 0);
 }
 
 export default { 1: part1, 2: part2 };
